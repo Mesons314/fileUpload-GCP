fdcb86f90ccc81d6db35f068dbf02e1a008e5d42
Mesons314 <143166351+Mesons314@users.noreply.github.com>
2025-02-09 22:34:08 +0530

    Update BucketUtil.java

--- src/main/java/com/xeroKarao/demo/util/BucketUtil.java
+++ src/main/java/com/xeroKarao/demo/util/BucketUtil.java
@@ -6,81 +6,85 @@ import com.xeroKarao.demo.dto.FileDto;
 import com.xeroKarao.demo.exception.FileWriteException;
 import com.xeroKarao.demo.exception.GCPFileUploadException;
 import com.xeroKarao.demo.exception.InvalidFileTypeException;
-import lombok.Value;
-import org.apache.coyote.BadRequestException;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
+import com.xeroKarao.demo.exception.BadRequestException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.SecureRandom;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.Constants.CHARACTERS;
-
-@Component
+@Service
 public class BucketUtil {
 
-    //Line 24 to 35 are present in the
-    //resources -> application.properties
-    //there you have to do the gcp configuration
-    @Value("${gcp.config.file}")
-    private String gcpConfigFile;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    @Value("${gcp.project.id}")//the id of the project in gcp would come
-    private String gcpProjectId;
+    private final String gcpConfigFile;
+    private final String gcpProjectId;
+    private final String gcpBucketId;
+    private final String gcpDirectoryName;
 
-    @Value("${gcp.bucket.id}")
-    private String gcpBucketId;
+    private static final String[] ALLOWED_EXTENSIONS = {".png", ".jpeg", ".pdf", ".doc"};
+    private Storage storage; // Store the GCP Storage object to reuse it
 
-    @Value("${gcp.dir.name}")
-    private String gcpDirectoryName;
-
-    private static final String[] ALLOWED_EXTENSIONS = {".png", ".jpeg", ".pdf", ".doc", ".mp3"};
-
-
-    //Uploading the files to GCP and return its url
-    public FileDto uploadFile(MultipartFile multipartFile, String fileName, String contentType){
-
-        try (InputStream credentialStream = new ClassPathResource(gcpConfigFile).getInputStream()){
-            Storage storage = StorageOptions.newBuilder()
-                    .setCredentials(GoogleCredentials.fromStream(credentialStream))
-                    .build()
-                    .getService();
-
-            byte[] fileData = convertFile(multipartFile);
-
-            String uniqueFileName = generateUniqueName(fileName);
-
-            BlobId blobId = BlobId.of(gcpBucketId,gcpDirectoryName+" / " + uniqueFileName);
-
-            //Creating the metadata of the file
-            BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
-                    .setContentType(contentType)
-                    .build();
-
-            //this part is uploading the file to gcp
-            Blob blob = storage.create(blobInfo,fileData);
-
-            //returning the file info
-            if(blob != null){
-                return new FileDto(blob.getName(),blob.getMediaLink());
-            }
-        } catch (IOException e) {
-            throw  new GCPFileUploadException("An error has occured");
-        }
-        throw  new GCPFileUploadException("An error has occured");
+    // Constructor-based dependency injection
+    public BucketUtil(
+            @Value("${spring.cloud.gcp.credentials.location}") String gcpConfigFile,
+            @Value("${spring.cloud.gcp.project-id}") String gcpProjectId,
+            @Value("${gcp.bucket.id}") String gcpBucketId,
+            @Value("${gcp.dir.name}") String gcpDirectoryName) {
+        this.gcpConfigFile = gcpConfigFile.replace("file:", "");
+        this.gcpProjectId = gcpProjectId;
+        this.gcpBucketId = gcpBucketId;
+        this.gcpDirectoryName = gcpDirectoryName;
     }
 
-    //Converting The file to bytes
-    private byte[] convertFile(MultipartFile file){
-        try{
-            if(file.getOriginalFilename() == null){
-                throw new BadRequestException("This is not running");
+    // Initialize GCP Storage after Spring sets up the beans
+    @PostConstruct
+    private void initializeStorage() throws IOException {
+        try (InputStream credentialStream = new FileInputStream(gcpConfigFile)) {
+            this.storage = StorageOptions.newBuilder()
+                    .setCredentials(GoogleCredentials.fromStream(credentialStream))
+                    .setProjectId(gcpProjectId)
+                    .build()
+                    .getService();
+        }
+    }
+
+    // Uploading the files to GCP and return its URL
+    public FileDto uploadFile(MultipartFile multipartFile, String fileName, String contentType) throws IOException {
+        byte[] fileData = convertFile(multipartFile);
+
+        String uniqueFileName = generateUniqueName(fileName);
+        BlobId blobId = BlobId.of(gcpBucketId, gcpDirectoryName + "/" + uniqueFileName);
+
+        // Creating the metadata of the file
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setContentType(contentType)
+                .build();
+
+        // Uploading the file to GCP
+        Blob blob = storage.create(blobInfo, fileData);
+
+        // Returning the file info
+        if (blob != null) {
+            return new FileDto(blob.getName(), blob.getMediaLink());
+        }
+        throw new GCPFileUploadException("An error has occurred");
+    }
+
+    // Converting The file to bytes
+    private byte[] convertFile(MultipartFile file) {
+        try {
+            if (file.getOriginalFilename() == null) {
+                throw new BadRequestException("File name is missing");
             }
             return file.getBytes();
-        }catch (IOException e){
-            throw new FileWriteException("An error has occured while converting the file");
+        } catch (IOException e) {
+            throw new FileWriteException("An error has occurred while converting the file");
         }
     }
 
@@ -91,6 +95,7 @@ public class BucketUtil {
         String extension = checkFileExtension(fileName);
         return fileName + "-" + randomString + extension;
     }
+
     private static String generateRandomString(int length) {
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
@@ -100,18 +105,15 @@ public class BucketUtil {
     }
 
     private static String checkFileExtension(String fileName) {
-        if(fileName != null && fileName.contains(".")){
+        if (fileName != null && fileName.contains(".")) {
             String fileExtension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
 
-            for(String allowedExtensions:ALLOWED_EXTENSIONS ){
-                if(fileExtension.equals(allowedExtensions)){
+            for (String allowedExtensions : ALLOWED_EXTENSIONS) {
+                if (fileExtension.equals(allowedExtensions)) {
                     return allowedExtensions;
                 }
             }
         }
-
-        throw new InvalidFileTypeException("something is wrong");
+        throw new InvalidFileTypeException("Invalid file type");
     }
-
-
 }
